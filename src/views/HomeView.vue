7781352b610602fad7b9ef13ca28f0ae28c877ae
serety95 <serety95@gmail.com>
2022-07-16 20:06:09 +0200

    home page update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
     <div class="container">
-      <div class="data-container">
-        <h1>why prexle</h1>
-        <p>
-          Prexle is a Cloud-Based Point-of-Sale (Cashier) System, designed specifically to meet the needs of the retail
-          sector in sales operations and inventory management in order to achieve business goals smoothly and safely,
-          wherever you may be.
-        </p>
-      </div>
-      <div class="img-container">
-        <img src="https://prexle.com/wp-content/uploads/2021/10/ipad.png" alt="" srcset="" />
+      <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+          <div class="data-container">
+            <h1>why prexle</h1>
+            <p>
+              Prexle is a Cloud-Based Point-of-Sale (Cashier) System, designed specifically to meet the needs of the
+              retail sector in sales operations and inventory management in order to achieve business goals smoothly and
+              safely, wherever you may be.
+            </p>
+          </div>
+        </div>
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+          <div class="img-container">
+            <img src="https://prexle.com/wp-content/uploads/2021/10/ipad.png" alt="" srcset="" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -37,13 +43,15 @@ export default {
         font-size: 64px;
         margin-bottom: 30px;
       }
-      p{
+      p {
         color: white;
         line-height: 2;
       }
     }
-    .img-container{
-      img{
+    .img-container {
+      img {
+        width: 100%;
+
         max-height: 400px;
       }
     }
